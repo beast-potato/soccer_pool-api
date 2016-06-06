f7c5d96be3ec4979508996df2306ddd0930a5f38
brian <brian@plasticmobile.com>
2016-06-06 02:02:56 -0400

    test functions working

--- src/testapi.rb
+++ src/testapi.rb
@@ -1,3 +1,39 @@
+get '/test/setup' do 
+    game0 = {}
+    game0["gameID"] = "0"
+    game0["awayTeam"] = "TEAM A"
+    game0["homeTeam"] = "TEAM B"
+    game0["startTime"] = 1465617600
+    game0["awayGoals"] = 0
+    game0["homeGoals"] = 0
+
+    game1 = {}
+    game1["gameID"] = "1"
+    game1["awayTeam"] = "TEAM A"
+    game1["homeTeam"] = "TEAM B"
+    game1["startTime"] = 1465531200
+    game1["awayGoals"] = 0
+    game1["homeGoals"] = 0
+
+    game2 = {}
+    game2["gameID"] = "2"
+    game2["awayTeam"] = "TEAM A"
+    game2["homeTeam"] = "TEAM B"
+    game2["startTime"] = 1464840000
+    game2["awayGoals"] = 3
+    game2["homeGoals"] = 0
+
+    collection = ECMongo.getCollection("Games")
+    collection.drop
+    collection.insert_many([game0, game1, game2])
+
+    games = safeArray(collection.find().to_a)
+    result = defaultResult()
+    result["games"] = games
+    return formatResult(result)
+end
+
+
 post '/test/login' do
     result = defaultResult()
     
@@ -46,8 +82,15 @@ post '/test/login' do
 end
 
 get '/test/pool' do
+    ##Authentication
+    authInfo = tokenAuthentication(request.env)
+    if authInfo["success"] == false
+        return formatResult(authInfo)
+    end
+    user = authInfo
+    #Authentication
+
     result = defaultResult()
-    
     brian = {}
     brian["name"] = "Brian"
     brian["points"] = 10
@@ -70,52 +113,52 @@ get '/test/pool' do
 end
 
 get '/test/games' do
+    ##Authentication
+    authInfo = tokenAuthentication(request.env)
+    if authInfo["success"] == false
+        return formatResult(authInfo)
+    end
+    user = authInfo
+    #Authentication
+
     result = defaultResult()
+    
+    collection = ECMongo.getCollection("Games")
+    games = safeArray(collection.find().to_a)
+    gamesHash = {}
 
-    prediction0 = {}
-    prediction0["awayGoals"] = 0
-    prediction0["awayGoals"] = 0
+    collection = ECMongo.getCollection("Predictions")
+    predictions = safeArray(collection.find({"token" => user["token"]}).to_a)
+    predictionsHash = {}
+    for prediction in predictions 
+        predictionsHash[prediction["gameID"]] = prediction
+    end
 
-    prediction1 = {}
-    prediction1["awayGoals"] = 3
-    prediction1["awayGoals"] = 0
+    gamePredictions = []
+    for game in games 
+        prediction = predictionsHash[game["gameID"]]
+        if prediction.nil?
+            prediction = {}
+            prediction["awayGoals"] = 0
+            prediction["homeGoals"] = 0
+        end
+        game["prediction"] = prediction
+        gamePredictions.push(game)
+    end
 
-    prediction2 = {}
-    prediction2["awayGoals"] = 2
-    prediction2["awayGoals"] = 1
-
-    game0 = {}
-    game0["gameID"] = "0"
-    game0["awayTeam"] = "TEAM A"
-    game0["homeTeam"] = "TEAM B"
-    game0["startTime"] = "2016-06-11T18:00:00Z"
-    game0["awayGoals"] = 0
-    game0["homeGoals"] = 0
-    game0["prediction"] = prediction0
-
-    game1 = {}
-    game1["gameID"] = "1"
-    game1["awayTeam"] = "TEAM A"
-    game1["homeTeam"] = "TEAM B"
-    game1["startTime"] = "2016-06-10T18:00:00Z"
-    game1["awayGoals"] = 0
-    game1["homeGoals"] = 0
-    game1["prediction"] = prediction1
-
-    game2 = {}
-    game2["gameID"] = "2"
-    game2["awayTeam"] = "TEAM A"
-    game2["homeTeam"] = "TEAM B"
-    game2["startTime"] = "2016-06-02T18:00:00Z"
-    game2["awayGoals"] = 3
-    game2["homeGoals"] = 0
-    game2["prediction"] = prediction2
-
-    result["data"] = [game0, game1, game2]
+    result["data"] = gamePredictions
     return formatResult(result)
 end
 
 post '/test/predictgame' do
+    ##Authentication
+    authInfo = tokenAuthentication(request.env)
+    if authInfo["success"] == false
+        return formatResult(authInfo)
+    end
+    user = authInfo
+    #Authentication
+    
     result = defaultResult()
 
     #validate params
@@ -144,5 +187,56 @@ post '/test/predictgame' do
         return Error(ECError["InvalidInput"], "game 'gameID' does not exist")
     end
 
+    collection = ECMongo.getCollection("Predictions")
+    prediction = {}
+    prediction["gameID"] = gameID
+    prediction["awayGoals"] = awayGoals
+    prediction["homeGoals"] = homeGoals
+    prediction["token"] = user["token"]
+    
+    collection.delete_one({"gameID" => gameID, "token" => user["token"]}).to_a
+    collection.insert_one(prediction)
+
+    result["data"] = safeObj(prediction)
     return formatResult(result)
 end    
+
+def tokenAuthentication(requestInfo)
+## token authentication
+    token = requestInfo["TOKEN"]
+    if token.nil?
+        token = requestInfo["HTTP_TOKEN"]
+    end
+
+    if token.nil?
+        return JSON.parse(Error(ECError["InvalidInput"], "'token' header required"))
+    end
+
+    collection = ECMongo.getCollection("Users")
+    results = collection.find({"token" => token}).to_a
+
+    if (results.length != 1)
+        return JSON.parse(Error(ECError["InvalidInput"], "invalid token"))
+    end
+    
+    user = results[0]
+    ## token authentication
+    return user
+end
+
+def safeArray(array)
+    arr = []
+    for obj in array
+        obj.delete("_id")
+        obj.delete("token")
+        arr.push(obj)
+    end 
+    return arr
+end
+
+def safeObj(obj)
+    obj.delete("_id")
+    obj.delete("token")
+    return obj
+end
+    
