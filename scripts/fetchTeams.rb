2d8d86fdf21e0089cb0093ecf491740eeb8acbdb
Brian Troy <brian@plasticmobile.com>
2016-06-06 17:37:15 -0400

    base of fetch scripts

---
+++ scripts/fetchTeams.rb
@@ -0,0 +1,28 @@
+require 'json'
+require 'net/http'
+require 'rsvg2'
+require 'nokogiri'
+
+uri = URI('http://api.football-data.org/v1/soccerseasons/424/teams')
+data = JSON.parse(Net::HTTP.get(uri))
+
+teamList = []
+
+teams = data["teams"]
+for team in teams
+	url = URI(team["crestUrl"])
+    teamObj = {}
+    teamObj["_id"] = team["_links"]["self"]["href"].split("/").last
+    teamObj["name"] = team["name"]
+    teamObj["image"] = "http://104.131.118.14/images/" + team["name"] + ".png"
+    
+    teamList.push(teamObj)
+    #don't break
+    break;
+end
+
+puts teamList
+
+# drop collection and insert_many(teamList)
+
+
